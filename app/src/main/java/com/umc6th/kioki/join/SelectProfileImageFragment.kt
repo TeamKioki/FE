9b48ae8dbbc8eb6d3dff639c66ea1ff425c2e422
MinhoiKoo <minlove2013@gmail.com>
2024-08-12 01:37:36 +0900

    feat: 캐릭터 선택 시 ImageView에 반영되도록 구현

--- app/src/main/java/com/umc6th/kioki/join/SelectProfileImageFragment.kt
+++ app/src/main/java/com/umc6th/kioki/join/SelectProfileImageFragment.kt
@@ -27,8 +27,34 @@ class SelectProfileImageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        selectCharacter()
+
         binding.nextButton.setOnClickListener {
             findNavController().navigate(R.id.action_selectProfileImageFragment_to_inputMemberIntroduceFragment)
+
+        }
+    }
+
+    private fun selectCharacter() {
+        binding.apply {
+            character1.setOnClickListener {
+                binding.selectProfileImageButton.setImageResource(R.drawable.profile1png)
+            }
+
+            character2.setOnClickListener {
+                binding.selectProfileImageButton.setImageResource(R.drawable.profile2)
+
+            }
+
+            character3.setOnClickListener {
+                binding.selectProfileImageButton.setImageResource(R.drawable.profile3)
+
+            }
+
+            character4.setOnClickListener {
+                binding.selectProfileImageButton.setImageResource(R.drawable.profile4)
+
+            }
         }
     }
 }
