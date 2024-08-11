1d81758702c9e5eb4e9fc88932df125c8295b64c
MinhoiKoo <minlove2013@gmail.com>
2024-08-12 01:36:36 +0900

    feat: 회원가입 완료 화면 구현
    - 로그인 창으로 이동하도록 변경 예정

---
+++ app/src/main/java/com/umc6th/kioki/join/ShowLoginFragment.kt
@@ -0,0 +1,26 @@
+package com.umc6th.kioki.join
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umc6th.myapplication.R
+
+class ShowLoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_show_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
