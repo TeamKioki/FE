96807296368671b98f06701d1671e3fa74308c22
MinhoiKoo <minlove2013@gmail.com>
2024-08-15 14:04:47 +0900

    refactor: layout 수정

--- app/src/main/java/com/umc6th/kioki/tutorial/practice/BurgerKingTutorialActivity.kt
+++ app/src/main/java/com/umc6th/kioki/tutorial/practice/BurgerKingTutorialActivity.kt
@@ -1,12 +1,11 @@
-package com.umc6th.kioki.tutorial
+package com.umc6th.kioki.tutorial.practice
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.umc6th.myapplication.R
+import com.umc6th.kioki.R
 
 class BurgerKingTutorialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
