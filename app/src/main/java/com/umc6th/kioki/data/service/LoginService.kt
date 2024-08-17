851e14a7eaff2e49173228177c6c4f051b4f6a59
MinhoiKoo <minlove2013@gmail.com>
2024-08-18 06:16:45 +0900

    refactor: code refactor

--- app/src/main/java/com/umc6th/kioki/data/service/LoginService.kt
+++ app/src/main/java/com/umc6th/kioki/data/service/LoginService.kt
@@ -1,7 +1,7 @@
 package com.umc6th.kioki.data.service
 
-import com.umc6th.kioki.data.response.CommonResponse
-import com.umc6th.kioki.data.response.TokenResponse
+import com.umc6th.kioki.data.network.response.CommonResponse
+import com.umc6th.kioki.data.network.response.TokenResponse
 import retrofit2.Response
 import retrofit2.http.POST
 
