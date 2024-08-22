dd7ea299151146e356043a9604750c64671e4140
MinhoiKoo <minlove2013@gmail.com>
2024-08-23 02:49:20 +0900

    refactor: 화살표 수정

--- app/src/main/java/com/umc6th/kioki/join/ShowLoginFragment.kt
+++ app/src/main/java/com/umc6th/kioki/join/ShowLoginFragment.kt
@@ -1,13 +1,20 @@
 package com.umc6th.kioki.join
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.umc6th.kioki.MainActivity
 import com.umc6th.kioki.R
+import com.umc6th.kioki.databinding.FragmentShowLoginBinding
+import com.umc6th.kioki.tutorial.BurgerKingRealActivity
 
 class ShowLoginFragment : Fragment() {
+    private lateinit var binding: FragmentShowLoginBinding
+    private val viewModel: JoinViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,11 +23,15 @@ class ShowLoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_login, container, false)
+        binding = FragmentShowLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.nextButton.setOnClickListener {
+            viewModel.executeJoin()
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
     }
 }
