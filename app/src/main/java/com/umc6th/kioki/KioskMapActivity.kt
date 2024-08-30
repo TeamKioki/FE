5202d786843717788404f7ac6cc0d517af06f01b
Jihyun <hjh20030615@gmail.com>
2024-08-31 00:32:11 +0900

    feat: 키오스크 지도 띄우기

--- app/src/main/java/com/umc6th/kioki/KioskMapActivity.kt
+++ app/src/main/java/com/umc6th/kioki/KioskMapActivity.kt
@@ -1,77 +1,95 @@
+package com.umc6th.kioki
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
+import com.umc6th.kioki.databinding.ActivityKioskmapBinding
 
-//package com.umc6th.kioki
-//
-//import android.content.pm.PackageManager
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.app.ActivityCompat
-//import androidx.core.content.ContextCompat
-//import androidx.databinding.DataBindingUtil
-//import com.umc6th.kioki.databinding.ActivityKioskmapBinding
-//
-//class KioskMapActivity : AppCompatActivity(), OnMapReadyCallback {
-//    private val LOCATION_PERMISSION_REQUEST_CODE = 5000
-//
-//    private val PERMISSIONS = arrayOf(
-//        Manifest.permission.ACCESS_FINE_LOCATION,
-//        Manifest.permission.ACCESS_COARSE_LOCATION
-//    )
-//
-//    private lateinit var binding: ActivityKioskmapBinding
-//    private lateinit var naverMap: NaverMap
-//    private lateinit var locationSource: FusedLocationSource
-//
-//    // onCreate에서 권한을 확인하며 위치 권한이 없을 경우 사용자에게 권한을 요청한다.
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_kioskmap)
-//
-//        if (!hasPermission()) {
-//            ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE)
-//        } else {
-//            initMapView()
-//        }
-//    }
-//
-//    private fun initMapView() {
-//        val fm = supportFragmentManager
-//        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
-//            ?: MapFragment.newInstance().also {
-//                fm.beginTransaction().add(R.id.map_fragment, it).commit()
-//            }
-//
-//        // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다.
-//        mapFragment.getMapAsync(this)
-//        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-//    }
-//
-//    // hasPermission()에서는 위치 권한이 있을 경우 true를, 없을 경우 false를 반환한다.
-//    private fun hasPermission(): Boolean {
-//        for (permission in PERMISSIONS) {
-//            if (ContextCompat.checkSelfPermission(this, permission)
-//                != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                return false
-//            }
-//        }
-//        return true
-//    }
-//
-//    override fun onMapReady(naverMap: NaverMap) {
-//        this.naverMap = naverMap
-//        // 현재 위치
-//        naverMap.locationSource = locationSource
-//        // 현재 위치 버튼 기능
-//        naverMap.uiSettings.isLocationButtonEnabled = true
-//        // 위치를 추적하면서 카메라도 따라 움직인다.
-//        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-//    }
-//}
+class KioskMapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val LOCATION_PERMISSION_REQUEST_CODE = 5000
 
-class KioskMapActivity : AppCompatActivity() {
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    private lateinit var binding: ActivityKioskmapBinding
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
+    // onCreate에서 권한을 확인하며 위치 권한이 없을 경우 사용자에게 권한을 요청한다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_kioskmap)
+
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("fameqf3adq")
+
+        if (!hasPermission()) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            initMapView()
+        }
+
+//        //마커 표시 예시 -> 하니까 안됨
+//        val marker = Marker()
+//        marker.position = LatLng(37.5670135, 126.9783740)
+//        marker.map = naverMap
+//        marker.icon = OverlayImage.fromResource(R.drawable.burgerking)
+//        marker.icon = MarkerIcons.BLACK
+//        marker.iconTintColor = Color.WHITE
+//        marker.width = 40
+//        marker.height = 43
+//        marker.width = Marker.SIZE_AUTO
+//        marker.height = Marker.SIZE_AUTO
+    }
+
+    private fun initMapView() {
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.kioskmap_map_vw) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.kioskmap_map_vw, it).commit()
+            }
+
+        // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다.
+        mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    // hasPermission()에서는 위치 권한이 있을 경우 true를, 없을 경우 false를 반환한다.
+    private fun hasPermission(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        // 현재 위치
+        naverMap.locationSource = locationSource
+        // 현재 위치 버튼 기능
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        // 위치를 추적하면서 카메라도 따라 움직인다.
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 }
